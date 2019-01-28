df9d7842ca45a50fb70eae650da3a37f8b31068e
Clay Murphree <clay@tropo.org>
2019-01-28 16:36:05 +0000

    Daily work for 1/28/19

--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
-console.log("Hello we are here");
+const http = require("http")//how we require modules
+
+http.createServer(function(request, response){
+    response.writeHead(200);
+    response.write("<html><body><h1>Dog is running </h1></body></html>");
+    setTimeout(function(){
+        response.write("<html><body><h2>Dog is done</h2></body></html>");
+        response.end();
+    }, 5000);
+}).listen(8080);
+
+console.log('listening on port 8080...');
